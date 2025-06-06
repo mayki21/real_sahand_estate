852aba68e2c041614d9a0f032cc37748054c6a06
mayki21 <mayank.khobragade9891@gmail.com>
2025-06-07 00:10:22 +0530

    made route & pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,11 +34,24 @@
 
 // export default App
 
-
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import SignUp from './pages/Signup'
+import Profile from './pages/Profile'
+import About from './pages/About'
+// import React from 'react'
 
 export default function App() {
   return (
-    <div className='text-red-700 font-bold'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
