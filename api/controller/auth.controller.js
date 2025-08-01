f7bba48c5ffc83f64bc4faec643a7073fd2ebbc0
mayki21 <mayank.khobragade9891@gmail.com>
2025-08-01 23:06:32 +0530

    error handler added

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../model/user.model.js"
 import bcrypt from "bcryptjs"
+import { errorHandler } from "../utils/error.js"
 
-export const signup=  async(req, res) => {
+export const signup=  async(req, res,next) => {
     const { username, email, password } = req.body
     const hashPassword = bcrypt.hashSync(password, 10)
     const newUser = new User({ username, email, password : hashPassword })
@@ -9,6 +10,6 @@ export const signup=  async(req, res) => {
         await newUser.save()
         return res.status(200).json({message:"User Created Successfully"})
     } catch (error) {
-        return res.status(500).json({message:error.message})
+        next(errorHandler(500, error.message))
     }
 }
--- api/index.js
+++ api/index.js
@@ -18,6 +18,17 @@ mongoose.connect(process.env.url).then(() => {
 app.use("/api/user", userRoute)
 app.use("/api/auth", authRoute)
 
+
+app.use((err,req,res,next)=>{
+    const statusCode =  err.statusCode || 500
+    const message = err.message || "Internal SERVER ERROR"
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
+
 app.listen(3000, () => {
     console.log(`Server is running on port ${process.env.port}`)
 })
